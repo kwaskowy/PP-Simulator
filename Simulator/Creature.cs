b2773f0dca7821b3e16eb3cd0c610b40fb05794e
Krystian Paryła <krystian.paryla@o2.pl>
2024-12-12 18:07:44 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,25 @@
+﻿namespace Simulator;
+
+internal class Creature
+{
+    private string Name { get; set; }
+    private int Level { get; set; }
+
+  
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    
+    public Creature() { }
+
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I am {Name} at level {Level}.");
+    }
+
+    public string Info => $"{Name} [{Level}]";
+}
