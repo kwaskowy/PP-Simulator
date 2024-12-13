aaf9323508db14ed11ed0c30e37ae8161d4437eb
Krystian Paryła <krystian.paryla@o2.pl>
2024-12-13 21:42:47 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -2,7 +2,21 @@
 
 public class Animals
 {
-    public required string Description { get; init; }
+    private string _description = "Default";
+
+    public required string Description
+    {
+        get => _description;
+        init
+        {
+            value = value.Trim();
+            if (value.Length < 3) value = value.PadRight(3, '#');
+            if (value.Length > 15) value = value.Substring(0, 15).TrimEnd();
+            if (value.Length < 3) value = value.PadRight(3, '#');
+            _description = char.ToUpper(value[0]) + value.Substring(1);
+        }
+    }
     public uint Size { get; set; } = 3;
+
     public string Info => $"{Description} <{Size}>";
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,25 +1,52 @@
 ﻿namespace Simulator;
 
 internal class Creature
-{
-    private string Name { get; set; }
-    private int Level { get; set; }
+{ 
+    private string _name = "Unknown";
+    private int _level = 1;
 
-  
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (_name != "Unknown") throw new InvalidOperationException("Name can only be set once.");
+            value = value.Trim();
+            if (value.Length < 3) value = value.PadRight(3, '#');
+            if (value.Length > 25) value = value.Substring(0, 25).TrimEnd();
+            if (value.Length < 3) value = value.PadRight(3, '#');
+            _name = char.ToUpper(value[0]) + value.Substring(1);
+        }
+    }
+
+    public int Level
+    {
+        get => _level;
+        set
+        {
+            if (_level != 1) throw new InvalidOperationException("Level can only be set once.");
+            _level = Math.Clamp(value, 1, 10);
+        }
+    }
+
     public Creature(string name, int level = 1)
     {
         Name = name;
         Level = level;
     }
 
-    
     public Creature() { }
 
+    public string Info => $"{Name} [{Level}]";
 
     public void SayHi()
     {
         Console.WriteLine($"Hi, I am {Name} at level {Level}.");
     }
 
-    public string Info => $"{Name} [{Level}]";
+    public void Upgrade()
+    {
+        if (Level < 10) _level++;
+    }
+
 }
