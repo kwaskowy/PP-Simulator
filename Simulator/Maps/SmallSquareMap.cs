681a4e9e34411aefe494f6e4e5105aa8b29be667
Krystian Paryła <krystian.paryla@o2.pl>
2024-12-21 22:00:36 +0100

    Mapy

---
+++ Simulator/Maps/SmallSquareMap.cs
@@ -0,0 +1,31 @@
+﻿namespace Simulator.Maps;
+
+public class SmallSquareMap : Map
+{
+    public int Size { get; }
+    public SmallSquareMap(int size)
+    {
+        if (size < 5 || size > 20)
+        {
+            throw new ArgumentOutOfRangeException(nameof(size), "Size must be between 5 and 20.");
+        }
+
+        Size = size;
+    }
+
+    public override bool Exist(Point p)
+    {
+        return p.X >= 0 && p.X < Size && p.Y >= 0 && p.Y < Size;
+    }
+
+    public override Point Next(Point p, Direction d)
+    {
+        Point next = p.Next(d);
+        return Exist(next) ? next : p;
+    }
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        Point next = p.NextDiagonal(d);
+        return Exist(next) ? next : p;
+    }
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Simulator;
+﻿using Simulator.Maps;
+
+namespace Simulator;
 
 internal class Program
 {
@@ -24,9 +26,30 @@ internal class Program
         Console.WriteLine(rect3.Contains(new Point(15, 5)));
         Console.WriteLine(rect3.Contains(new Point(10, 10)));
     }
+    static void Lab5b()
+    {
+        try
+        {
+            SmallSquareMap map = new SmallSquareMap(10);
+            Point p = new Point(5, 5);
+
+            Console.WriteLine(map.Exist(p));
+            Console.WriteLine(map.Next(p, Direction.Right));
+            Console.WriteLine(map.Next(p, Direction.Up));
+            Console.WriteLine(map.Next(new Point(9, 9), Direction.Right));
+            Console.WriteLine(map.NextDiagonal(p, Direction.Up));
+
+            SmallSquareMap invalidMap = new SmallSquareMap(25);
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         Lab5a();
+        Lab5b();
     }
 }
